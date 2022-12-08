1dd03984d6d9eb99f24a4d60a22c37832b734529
alexiangwa <achaalexiangwa@yahoo.com>
2022-12-07 19:37:27 -0600

    boolean operation

---
+++ boolean_operation.py
@@ -0,0 +1,7 @@
+my_str="Python"
+print(my_str.startswith('p'))
+print(my_str.startswith('pyth'))
+print(my_str.endswith('hon'))
+print(my_str.islower())
+print(my_str.isupper())
+print(my_str.istitle())
