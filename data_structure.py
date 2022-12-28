29df14537c60504d019e9343da43400aaac9ebb9
alexiangwa <achaalexiangwa@yahoo.com>
2022-12-28 11:39:00 -0600

    data structure

--- data_structure.py
+++ data_structure.py
@@ -2,6 +2,6 @@
 #there are four built in data structures 
 #there are list-->[],tuple-->(), dictinary-->{} with key value pair, set-->{}
 x=5
-print(x)
+print(x) 
 my_values=[3,4,5,'python','devops',5.6]
 print()
